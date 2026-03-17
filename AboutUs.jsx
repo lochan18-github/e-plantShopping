02dfe6ab5925e42c490ccf88d4e1a990e9ab91f5
lochan18-github <lochan.111100@gmail.com>
2026-03-18 01:25:57 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,30 @@
+// AboutUs.jsx
+
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Our Company</h1>
+      <p>
+        Our company is focused on building modern and user-friendly web
+        applications using the latest technologies such as React, JavaScript,
+        and modern front-end frameworks.
+      </p>
+
+      <p>
+        We aim to create scalable, efficient, and responsive applications that
+        provide a great user experience. Our team is passionate about software
+        development and continuously learning new technologies.
+      </p>
+
+      <p>
+        Our mission is to deliver high-quality digital solutions and help
+        businesses improve their online presence through innovative web
+        development.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
